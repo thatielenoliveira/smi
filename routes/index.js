e62890ba4374df9fd75ec58ae15163b818d6537b
Thatielen Oliveira <thatielenoliveira@hotmail.com>
2020-09-14 09:45:41 -0400

    feat: add theme and create patient template

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+  logging: false
+});
+
+var PatientModel = require('../models/patient/patient')(sequelize, Sequelize);
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('login');
-  res.send('index')
+  res.send('index');
 });
 
+router.get('/createPatient', function(req, res, next) {
+  //res.render('login');
+  res.render('createPatient');
+});
 
+router.post('/createPatient', function(req, res, next) {
+
+
+  sequelize.sync({ force: true }).then(() => {
+    PatientModel.create({
+      fullName: req.body.fullName,
+      mothersName: req.body.mothersName,
+      birthDate: req.body.birthDate,
+      gender: req.body.gender,
+      postalCode: req.body.postalCode,
+      streetName: req.body.streetName,
+      houseNumber: req.body.houseNumber,
+      complement: req.body.complement,
+      district: req.body.district,
+      city: req.body.city,
+      state: req.body.state,
+      phone: req.body.phone,
+      liveWith: req.body.liveWith,
+      emergencyPhone: req.body.emergencyPhone
+    }).then((user) => {
+        console.log('Patient Successfully Created');
+        res.redirect('/');
+    }, (error) => {
+        console.error(error);
+        res.redirect('/createPatient');
+    });
+});
+});
 
 
 module.exports = router;

ce8d561896189674ad2d94eb79a0eab0c80de880
Thatielen Oliveira <thatielenoliveira@hotmail.com>
2020-09-14 11:26:55 -0400

    feat: add patient edition

--- routes/index.js
+++ routes/index.js
@@ -15,19 +15,15 @@ var PatientModel = require('../models/patient/patient')(sequelize, Sequelize);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('login');
   res.send('index');
 });
 
 router.get('/createPatient', function(req, res, next) {
-  //res.render('login');
   res.render('createPatient');
 });
 
 router.post('/createPatient', function(req, res, next) {
 
-
-  sequelize.sync({ force: true }).then(() => {
     PatientModel.create({
       fullName: req.body.fullName,
       mothersName: req.body.mothersName,
@@ -43,14 +39,63 @@ router.post('/createPatient', function(req, res, next) {
       phone: req.body.phone,
       liveWith: req.body.liveWith,
       emergencyPhone: req.body.emergencyPhone
-    }).then((user) => {
+    }).then((result) => {
         console.log('Patient Successfully Created');
         res.redirect('/');
     }, (error) => {
         console.error(error);
         res.redirect('/createPatient');
     });
+
 });
+
+router.get('/editPatient/:id', function(req, res, next) {
+
+  PatientModel.findAll({
+    limit: 1,
+    where: {
+      id: req.params.id
+    },
+  }).then(function(entries){
+    if (entries) {
+      res.render('editPatient', { data: entries});
+  } else {
+      console.log("no data exist for this id");
+  }
+    
+  }, (error) => {
+    console.error(error);
+}); 
+
+});
+
+router.post('/editPatient/:id', function(req, res, next) {
+
+    PatientModel.update({
+      fullName: req.body.fullName,
+      mothersName: req.body.mothersName,
+      birthDate: req.body.birthDate,
+      gender: req.body.gender,
+      postalCode: req.body.postalCode,
+      streetName: req.body.streetName,
+      houseNumber: req.body.houseNumber,
+      complement: req.body.complement,
+      district: req.body.district,
+      city: req.body.city,
+      state: req.body.state,
+      phone: req.body.phone,
+      liveWith: req.body.liveWith,
+      emergencyPhone: req.body.emergencyPhone
+    },
+    { where: { id: req.params.id } }
+    ).then((result) => {
+        console.log(result);
+        console.log('Patient Successfully Updated');
+        res.redirect('/');
+    }, (error) => {
+        console.error(error);
+    });
+
 });
 
 
